391c26c3f9d34cfe2a0f6fd5980c9f40e403fd8f
vzaepro <50335215+vzaepro@users.noreply.github.com>
2025-08-26 15:02:31 +0700

    Update ContactSection.tsx

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -287,7 +287,7 @@ const ContactSection = () => {
           <CardContent>
             <div className="aspect-video bg-accent/20 rounded-lg overflow-hidden">
               <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3954.654!2d112.42!3d-7.47!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e78182f5c123456%3A0x7890abcdef123456!2sJl.%20Sekarputih%2C%20Mergelo%2C%20Kedundung%2C%20Kec.%20Magersari%2C%20Kota%20Mojokerto%2C%20Jawa%20Timur%2061316!5e0!3m2!1sen!2sid!4v1234567890"
+                src="https://maps.app.goo.gl/QhcZ4hiUYFQfXCvz5"
                 width="100%"
                 height="100%"
                 style={{ border: 0 }}
